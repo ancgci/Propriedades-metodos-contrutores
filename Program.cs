3b839978ce7af0a22bdff775ed020d4fd12ec3f9
ancgci <sr.antoniocarlos@gmail.com>
2023-11-03 20:16:38 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa(nome:"Antonio", sobrenome: "Carlos");
+Pessoa p2 = new Pessoa(nome:"Garcia", sobrenome: "Junior");
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Inglês";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Antonio Carlos";
+// p1.Sobrenome = "Garcia Junior";
+// p1.Idade = 40;
+// p1.Apresentar();
